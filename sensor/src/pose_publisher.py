ff415d253dc95d641c6e6e75b9bf5528f967f5f0
songdh99 <songdh99@gmail.com>
2021-11-18 19:18:56 +0900

    publisher complete

---
+++ sensor/src/pose_publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from common_msg.msg import position
+
+rospy.init_node('pose_publisher')
+pub = rospy.Publisher('common_msg', position, queue_size=1)
+msg = position()
+count = 0
+rate = rospy.Rate(1)
+while not rospy.is_shutdown():
+    if count > 100:
+        count = 0    
+    msg.point.x = count * 10
+    msg.point.y = count + 10
+    msg.point.z = count / 2
+    msg.timestamp.data = count % 60 
+    pub.publish(msg)
+    print "published data of x: ", msg.point.x
+    print "published data of y: ", msg.point.y
+    print "published data of z: ", msg.point.z
+    print "published timestemp: ", msg.timestamp.data
+    print "Pub finish"
+
+    count += 1
+    rate.sleep()
