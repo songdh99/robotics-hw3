4cb0226d937214b4c238bd0e3d3809374f4c4afc
songdh99 <songdh99@gmail.com>
2021-11-18 19:16:50 +0900

    subscribed complete

---
+++ algorithm/src/pose_subscriber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from common_msg.msg import position
+
+def callback(msg):
+    print "subscribed position x: ", msg.point.x
+    print "subscribed position y: ", msg.point.y
+    print "subscribed position z: ", msg.point.z
+    print "Sub finish"
+
+rospy.init_node('pose_subscriber')
+sub = rospy.Subscriber('common_msg', position, callback)
+rospy.spin()
